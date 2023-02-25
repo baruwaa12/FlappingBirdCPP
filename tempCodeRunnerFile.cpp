948e9eb901a2221fbfac0159037735e880f870d3
baruwaa12 <baruwaademola@outlook.com>
2023-02-25 20:10:18 +0000

    Trying to get Build working with SDL

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,20 @@
+// This line renders the window to the screen
+    // SDL_Window *window = NULL;
+
+    // // The surface which is held by the window
+    // SDL_Surface *screenSurface = NULL;
+
+    // // Initialize SDL
+    // if (SDL_Init(SDL_INIT_VIDEO) < 0)
+    // {
+    //     // printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+    // }
+    // else
+    // {
+    //     // Create window
+    //     window = SDL_CreateWindow("SDL Tutorial", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    //     if (window == NULL)
+    //     {
+    //         // printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+    //     }
+    // }
